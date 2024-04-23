b68d055f60c7837534b98a2b4bde0d1a432008a6
Daniel Taki <danieltaki97@gmail.com>
2024-04-23 01:01:20 -0500

    added some csv

--- app/actions/action.ts
+++ app/actions/action.ts
@@ -1,59 +1,60 @@
 "use server";
 
-import { authSchema, optsSchema } from "@/types/optsSchema";
-import Shipstation from "shipstation-node";
-import type { IOrderPaginationResult, IOrder } from "shipstation-node/typings/models";
-import type { paths, components } from "@/types/book-freight/schema";
-import createClient from "openapi-fetch";
-import date from 'date-and-time';
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http'
-import * as schema from '@/app/db/drizzle/schema';
-import { eq } from 'drizzle-orm';
-import { Schema } from "zod";
+import * as schema from "@/app/db/drizzle/schema";
+import type { components, paths } from "@/types/book-freight/schema";
+import { optsSchema } from "@/types/optsSchema";
+import { neon } from "@neondatabase/serverless";
+import date from "date-and-time";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/neon-http";
 import { alias } from "drizzle-orm/pg-core";
-import { DrawerItems } from "flowbite-react";
-import { Component } from "react";
-
+import createClient from "openapi-fetch";
+import Shipstation from "shipstation-node";
+import type {
+  IOrder,
+  IOrderPaginationResult,
+} from "shipstation-node/typings/models";
 
 type EnrichedOrder = Awaited<ReturnType<typeof getEnrichedOrder>>;
 type EnrichedItem = Awaited<ReturnType<typeof getData>>;
 type Dimensions = Awaited<ReturnType<typeof parseDimensionsAndQty>>;
 type LTLItem = components["schemas"]["Rates.LTL.RateToBookRequest"]["items"];
-type FreightClass = components["schemas"]["FreightClass"]
+type FreightClass = components["schemas"]["FreightClass"];
+type PackagingType = components["schemas"]["LTLPackagingType"];
+type Hazard = components["schemas"]["HazardousMaterial"];
 
-
-async function getData(sku: string) { 
-  const hardCodedSku = 'M1G-9XQ-Q4C'
-  console.log('Getting data: ' + hardCodedSku)
+async function getData(sku: string) {
+  const hardCodedSku = "M1G-9XQ-Q4C";
+  console.log("Getting data: " + hardCodedSku);
   const sql = neon(process.env.DATABASE_URL!);
   const db = drizzle(sql, { schema });
-  
-  const freightLinks = alias(schema.productFreightLinks, 'freightLinks');
-  const freightClass = alias(schema.freightClassifications, 'freightClass')
+
+  const freightLinks = alias(schema.productFreightLinks, "freightLinks");
+  const freightClass = alias(schema.freightClassifications, "freightClass");
   const product = await db
     .select({
       product: schema.products,
       freightClassifications: freightLinks,
       freightClass: freightClass,
-      
-  })
+    })
     .from(schema.products)
     .innerJoin(
       freightLinks,
-      eq(schema.products.productId, freightLinks.productId)
+      eq(schema.products.productId, freightLinks.productId),
     )
     .innerJoin(
       freightClass,
-      eq(freightLinks.classificationId, freightClass.classificationId)
+      eq(freightLinks.classificationId, freightClass.classificationId),
     )
     .where(eq(schema.products.sku, hardCodedSku))
     .execute();
-    console.log(product);
-    return product;
+  console.log(product);
+  return product;
 }
 
-const client = createClient<paths>({ baseUrl: process.env.SANDBOX_FREIGHTVIEW_BASE_URL});
+const client = createClient<paths>({
+  baseUrl: process.env.SANDBOX_FREIGHTVIEW_BASE_URL,
+});
 
 interface ErrorResult {
   error: {
@@ -102,152 +103,172 @@ async function getEnrichedOrder(orderData: IOrderPaginationResult) {
   // Prepare to accumulate all enriched order data
   const order = orderData.orders[0];
 
-
   console.log(`Processing order: ${order.orderId}`);
-    
+
   // Array to hold enriched items for this order
   const enrichedItems = [];
 
-  for (const item of order.items) { 
+  for (const item of order.items) {
     const itemData = await getData(item.sku);
-    console.log(`Data for item ${item.sku} in order ${order.orderNumber}:`, itemData);
+    console.log(
+      `Data for item ${item.sku} in order ${order.orderNumber}:`,
+      itemData,
+    );
 
     enrichedItems.push({
       ...item,
-      additionalData: itemData
-    })
+      additionalData: itemData,
+    });
   }
   const enrichedOrder = {
-    ...order, 
-    enrichedItems: enrichedItems
+    ...order,
+    enrichedItems: enrichedItems,
   };
   return enrichedOrder;
 }
 
-
-
-export async function bookFreight(orderData: IOrderPaginationResult, liftgate: boolean, limitedAccess: boolean) {
+export async function bookFreight(
+  orderData: IOrderPaginationResult,
+  liftgate: boolean,
+  limitedAccess: boolean,
+) {
   try {
     if (!orderData || orderData.orders.length === 0) {
       console.log("No orders found");
-      return 'No orders available';
+      return "No orders available";
     }
     if (orderData.orders.length > 1) {
       console.log("Only one order can be processed at a time");
-      return 'Only one order can be processed at a time';
+      return "Only one order can be processed at a time";
     }
 
-      const enrichedOrder = await getEnrichedOrder(orderData);
-      console.log(enrichedOrder);
-      // const shipmentResult = await rateLtlShipment(EnrichedOrder, liftgate, limitedAccess)
-    } catch (error) {
+    const enrichedOrder = await getEnrichedOrder(orderData);
+    console.log(enrichedOrder);
+    // const shipmentResult = await rateLtlShipment(EnrichedOrder, liftgate, limitedAccess)
+  } catch (error) {
     console.log(error);
     return null;
   }
-};
+}
+
+function getHazard(items: EnrichedOrder): Hazard {
+
+}
 
 function validateFreightClass(freightClass: number | undefined): FreightClass {
-  const validClasses: number[] = [50, 55, 60, 65, 70, 77.5, 85, 92.5, 100, 110, 125, 150, 175, 200, 250, 300, 400, 500];
+  const validClasses: number[] = [
+    50, 55, 60, 65, 70, 77.5, 85, 92.5, 100, 110, 125, 150, 175, 200, 250, 300,
+    400, 500,
+  ];
   if (freightClass !== undefined && validClasses.includes(freightClass)) {
     return freightClass as FreightClass;
   } else {
-    throw new Error('Invalid freight class');
+    throw new Error("Invalid freight class");
   }
-    
-    
+
+  function getItems(
+    items: EnrichedOrder,
+    weight: number,
+    dimensions: Dimensions,
+  ): LTLItem {
+    let LTLitems: LTLItem;
+
+    for (let i = 0; i < dimensions.qty; i++) {
+      LTLitems.push({
+        description: items.product.name,
+        weight: weight,
+        freightClass: validateFreightClass(
+          parseInt(items.freightClass.freightClass, 10),
+        ),
+        length: dimensions.length,
+        width: dimensions.width,
+        height: dimensions.height,
+        package: items.enrichedItems[i].additionalData[i].product.packagingType as LTLPackagingType,
+        pieces: 1,
+        nmfc: items.enrichedItems[i].additionalData[i].freightClass.nmfc?,
+        hazardous: items.enrichedItems[i].additionalData[i].freightClass.hazardous?,
+        hazard: getHazard(),
 
 
-
-
-function getItems(items: EnrichedOrder, weight: number, dimensions: Dimensions): LTLItem {
-  let LTLitems: LTLItem;
-
-  // How many shipments are there in the order? => check the internal notes parse qty
- 
-  
-  items.forEach(item => {
-    LTLitems.forEach( ltlItem => {
-      ltlItem.description = item.product.name,
-      ltlItem.weight = weight,
-      ltlItem.freightClass = validateFreightClass(parseInt(item.freightClass.freightClass, 10)),
-      ltlItem.length = dimensions.length,
-      ltlItem.width = dimensions.width,
-      ltlItem.height = dimensions.height, 
-
-    })
-  })
-  return items;
-}
-  
-
-
-   
-
-
-async function rateLtlShipment(order: EnrichedOrder , liftgate: boolean, limitedAccess: boolean) {
-  const now = new Date();
-  const todayDate  = date.format(now, 'YYYY-MM-DD');
-  const weight = parseWeight(order) 
-  const dimensions = parseDimensionsAndQty(order);
-  let destType: "business dock" | "business no dock" | "residential" | "limited access" | "trade show" | "construction" | "farm" | "military" | "airport" | "place of worship" | "school" | "mine" | "pier" | undefined;
-  const items: LTLItem = getItems(order, weight, dimensions);
-  if (liftgate && !order.shipTo.residential) { 
-    destType = "business no dock";
-  } else if (order.shipTo.residential) {
-     destType = "residential";
-  } else if (limitedAccess) {
-    destType = "limited access";
-  } else {
-    destType = "business dock";
-  }
-  return await client.POST("/rates", {
-    body: {
-      pickupDate: todayDate,
-      charges: liftgate ? ["liftgate delivery"] : undefined,
-      originCompany: "Alliance Chemical",
-      originAddress: "204 South Edmond Street",
-      originCity: "Taylor",
-      originState: "TX",
-      originPostalCode: "76574",
-      originCountry: "USA",
-      originType: "business dock",
-      originContactName: "Adnan Heikal",
-      originContactPhone: "512-365-6838",
-      originContactEmail: "adnan.heikal@alliancechemical.com",
-      originReferenceNumber: order.orderNumber,
-      originDockHoursOpen: "09:00 AM",
-      originDockHoursClose: "04:00 PM",
-      destCompany: order.shipTo.company ? order.shipTo.company : undefined,
-      destAddress: order.shipTo.street1,
-      destAddress2: order.shipTo.street2 ? order.shipTo.street2 : undefined,  
-      destCity: order.shipTo.city,
-      destState: order.shipTo.state,
-      destPostalCode: order.shipTo.postalCode,
-      destCountry: "USA",
-      destType: destType,
-      destContactName: order.shipTo.name,
-      destContactPhone: order.shipTo.phone,
-      destContactEmail: order.customerEmail,
-      destReferenceNumber: order.orderNumber,
-      destDockHoursOpen: "09:00 AM",
-      destDockHoursClose: "04:00 PM",
-      billPostalCode: order.shipTo.postalCode,
-      billCountry: "USA",
-      items: [
-
-      ]
-
-
-
-
-      
-
-
+      });
     }
-  })
-}
 
-  
+
+    return items;
+  }
+
+  async function rateLtlShipment(
+    order: EnrichedOrder,
+    liftgate: boolean,
+    limitedAccess: boolean,
+  ) {
+    const now = new Date();
+    const todayDate = date.format(now, "YYYY-MM-DD");
+    const weight = parseWeight(order);
+    const dimensions = parseDimensionsAndQty(order);
+    let destType:
+      | "business dock"
+      | "business no dock"
+      | "residential"
+      | "limited access"
+      | "trade show"
+      | "construction"
+      | "farm"
+      | "military"
+      | "airport"
+      | "place of worship"
+      | "school"
+      | "mine"
+      | "pier"
+      | undefined;
+    const items: LTLItem = getItems(order, weight, dimensions);
+    if (liftgate && !order.shipTo.residential) {
+      destType = "business no dock";
+    } else if (order.shipTo.residential) {
+      destType = "residential";
+    } else if (limitedAccess) {
+      destType = "limited access";
+    } else {
+      destType = "business dock";
+    }
+    return await client.POST("/rates", {
+      body: {
+        pickupDate: todayDate,
+        charges: liftgate ? ["liftgate delivery"] : undefined,
+        originCompany: "Alliance Chemical",
+        originAddress: "204 South Edmond Street",
+        originCity: "Taylor",
+        originState: "TX",
+        originPostalCode: "76574",
+        originCountry: "USA",
+        originType: "business dock",
+        originContactName: "Adnan Heikal",
+        originContactPhone: "512-365-6838",
+        originContactEmail: "adnan.heikal@alliancechemical.com",
+        originReferenceNumber: order.orderNumber,
+        originDockHoursOpen: "09:00 AM",
+        originDockHoursClose: "04:00 PM",
+        destCompany: order.shipTo.company ? order.shipTo.company : undefined,
+        destAddress: order.shipTo.street1,
+        destAddress2: order.shipTo.street2 ? order.shipTo.street2 : undefined,
+        destCity: order.shipTo.city,
+        destState: order.shipTo.state,
+        destPostalCode: order.shipTo.postalCode,
+        destCountry: "USA",
+        destType: destType,
+        destContactName: order.shipTo.name,
+        destContactPhone: order.shipTo.phone,
+        destContactEmail: order.customerEmail,
+        destReferenceNumber: order.orderNumber,
+        destDockHoursOpen: "09:00 AM",
+        destDockHoursClose: "04:00 PM",
+        billPostalCode: order.shipTo.postalCode,
+        billCountry: "USA",
+        items: [],
+      },
+    });
+  }
+
   return;
 }
 
@@ -255,15 +276,19 @@ function parseWeight(order: IOrder): number {
   const weightRegex = /(\d+)\s*(LBS|pound|pounds)/i;
   const match = order.internalNotes.match(weightRegex);
   if (match && match[1]) {
-      const weight = parseFloat(match[1]);
-      return parseFloat(weight.toFixed(2));
+    const weight = parseFloat(match[1]);
+    return parseFloat(weight.toFixed(2));
   } else {
-      throw new Error('Invalid weight in internal notes');
+    throw new Error("Invalid weight in internal notes");
   }
 }
 
-
-function parseDimensionsAndQty(order: EnrichedOrder): { qty: number; length: number; width: number; height: number } {
+function parseDimensionsAndQty(order: EnrichedOrder): {
+  qty: number;
+  length: number;
+  width: number;
+  height: number;
+} {
   const regex = /(\d+)@(\d+)x(\d+)x(\d+)/; // Pattern to match 'Qty@Length x Width x Height'
   const match = order.internalNotes.match(regex);
 
@@ -280,37 +305,18 @@ function parseDimensionsAndQty(order: EnrichedOrder): { qty: number; length: num
       height: height,
     };
   } else {
-    throw new Error('Invalid dimensions and qty in internal notes');
+    throw new Error("Invalid dimensions and qty in internal notes");
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* V2 API Oauth token generation */
 
-// async function generateToken() { 
+// async function generateToken() {
 //   const token = await client.POST("/v2.0/auth/token", {
 //     body: {
 //       client_id: process.env.VERCEL_FREIGHTVIEW_CLIENT_ID || '',
 //       client_secret: process.env.VERCEL_FREIGHTVIEW_CLIENT_SECRET || '',
-//       grant_type: "client_credentials", 
+//       grant_type: "client_credentials",
 //     }
 //   });
 //   if (token.error) {
@@ -336,8 +342,6 @@ function parseDimensionsAndQty(order: EnrichedOrder): { qty: number; length: num
 //   const access_token = validatedToken.data.data.access_token;
 //   return access_token;
 // };
-
-
 
 // async function createShipment(access_token: string, orderData: IOrderPaginationResult, liftgate: boolean, freightProduct: FreightProductData) {
 
@@ -386,7 +390,7 @@ function parseDimensionsAndQty(order: EnrichedOrder): { qty: number; length: num
 //         closesAt: "16:00",
 //         contactName: orderData.orders[0]?.shipTo.name,
 //         contactPhone: orderData.orders[0]?.shipTo.phone,
-//         contactEmail: orderData.orders[0]?.customerEmail,  
+//         contactEmail: orderData.orders[0]?.customerEmail,
 //       },
 //   ],
 //   billTo: {
@@ -439,10 +443,4 @@ function parseDimensionsAndQty(order: EnrichedOrder): { qty: number; length: num
 //     contains: {},
 //     hazardous: {}
 
-    
-
 //   }
-
-
-  
-  
