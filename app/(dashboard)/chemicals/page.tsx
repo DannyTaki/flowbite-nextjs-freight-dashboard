eaae89ee820d6770f1c2a25b2256f97ce264d9a8
Daniel Taki <daniel@alliancechemical.com>
2024-05-13 14:19:51 -0500

    added a way to update chemical entries

--- app/(dashboard)/chemicals/page.tsx
+++ app/(dashboard)/chemicals/page.tsx
@@ -8,16 +8,17 @@ import { useState } from "react";
 
 
 interface ChemicalData {
-  classificationId: number;
-  description: string | null;
-  nmfc: string | null;
-  freightClass: string;
-  hazardous: boolean | null;
-  hazardId: string | null;
-  packingGroup: string | null;
-  sub: string | null;
-  // Add additional methods and properties required by drizzle-orm
+  classificationId?: number;
+  description?: string;  // Make description also optional
+  nmfc?: string | null;
+  freightClass?: string | null;
+  hazardous?: boolean | null;
+  hazardId?: string | null;
+  packingGroup?: string | null;
+  sub?: string | null;
 }
+
+
 
 export default function Chemicals() {
   const [openModal, setOpenModal] = useState(false);
@@ -37,9 +38,9 @@ export default function Chemicals() {
   if (isLoading) {
     return (
       <div className="flex justify-center items-center h-screen">
-      <Spinner className="w-36 h-36" />
-    </div>
-    ) 
+        <Spinner className="w-36 h-36" />
+      </div>
+    )
   }
 
 
@@ -47,43 +48,75 @@ export default function Chemicals() {
     <div className="overflow-x-auto">
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
         <Modal.Header>Update Chemical Entry</Modal.Header>
-          <Modal.Body>
+        <Modal.Body>
           <form className="p-4 md:p-5">
-                <div className="grid gap-4 mb-4 grid-cols-2">
-                    <div className="col-span-2">
-                        <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <input type="text" name="description" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required={true} value={selectedChemical?.description ? selectedChemical.description : null}  />
-                    </div>
-                    <div className="col-span-2 sm:col-span-1">
-                        <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                        <input type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required={true} />
-                    </div>
-                    <div className="col-span-2 sm:col-span-1">
-                        <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
-                        <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option selected={true}>Select category</option>
-                            <option value="TV">TV/Monitors</option>
-                            <option value="PC">PC</option>
-                            <option value="GA">Gaming/Console</option>
-                            <option value="PH">Phones</option>
-                        </select>
-                    </div>
-                    <div className="col-span-2">
-                        <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
-                        <textarea id="description"  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here"></textarea>                    
-                    </div>
+            <div className="grid gap-4 mb-4 grid-cols-2">
+              <div className="col-span-2">
+                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                <input onChange={(e) => {
+                  const updateChemical = { ...selectedChemical, description: e.target.value };
+                  setSelectedChemical(updateChemical);
+                }} type="text" name="description" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type description" required={true} value={selectedChemical?.description ? selectedChemical.description : ''} />
+              </div>
+            </div>
+            <div className="grid grid-cols-3 gap-3 mb-4">
+              <div className="col-span-3 sm:col-span-1">
+                <label htmlFor="nmfc" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NMFC</label>
+                <input onChange={(e) => {
+                  const updateChemical = { ...selectedChemical, nmfc: e.target.value };
+                  setSelectedChemical(updateChemical);
+                }} type="text" name="nmfc" id="nmfc" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type NMFC" required={true} value={selectedChemical?.nmfc ? selectedChemical.nmfc : ''} />
+              </div>
+              <div className="col-span-3 sm:col-span-1">
+                <label htmlFor="sub" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">SUB</label>
+                <input onChange={(e) => {
+                  const updateChemical = { ...selectedChemical, sub: e.target.value };
+                  setSelectedChemical(updateChemical);
+                }} type="text" name="sub" id="sub" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type SUB" required={true} value={selectedChemical?.sub ? selectedChemical.sub : ''} />
+              </div>
+              <div className="col-span-3 sm:col-span-1">
+                <label htmlFor="freight-class" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">SUB</label>
+                <input onChange={(e) => {
+                  const updateChemical = { ...selectedChemical, sub: e.target.value };
+                  setSelectedChemical(updateChemical);
+                }} type="text" name="freight-class" id="freight-class" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Freight Class" required={true} value={selectedChemical?.sub ? selectedChemical.sub : ''} />
+              </div>
+            </div>
+
+            <div className="grid grid-cols-3 gap-3 mb-4">
+              <div className="col-span-3 sm:col-span-1">
+                <label htmlFor="hazardous" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hazardous</label>
+                <select id="hazardous" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" >
+                  <option value="Yes" selected={true}>Yes</option>
+                  <option value="No">No</option>
+                </select>
                 </div>
-                <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                    Add new product
-                </button>
-            </form>
-          </Modal.Body>
+                <div className="col-span-3 sm:col-span-1">
+                  <label htmlFor="hazard-id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hazard ID</label>
+                  <input onChange={(e) => {
+                    const updateChemical = { ...selectedChemical, hazardId: e.target.value };
+                    setSelectedChemical(updateChemical);
+                  }} type="text" name="hazardid" id="hazardid" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type SUB" required={true} value={selectedChemical?.hazardId ? selectedChemical.hazardId : ''} />
+                </div>
+                <div className="col-span-3 sm:col-span-1">
+                  <label htmlFor="packing-group" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Packing Group</label>
+                  <input onChange={(e) => {
+                    const updateChemical = { ...selectedChemical, packingGroup: e.target.value };
+                    setSelectedChemical(updateChemical);
+                  }} type="text" name="packing-group" id="packing-group" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Packing Group" required={true} value={selectedChemical?.packingGroup ? selectedChemical.packingGroup : ''} />
+                </div>
+            </div>
+              {/* <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                Update Product
+              </button> */}
+          </form>
+        </Modal.Body>
         <Modal.Footer>
-          <Button onClick={() => setOpenModal(false)}>I accept</Button>
-          <Button color="gray" onClick={() => setOpenModal(false)}>
+          <Button onClick={() => setOpenModal(false)}>Update Entry</Button>
+          {/* <Button color="gray" onClick={() => setOpenModal(false)}>
             Decline
-          </Button>
+          </Button> */}
         </Modal.Footer>
 
       </Modal>
@@ -99,7 +132,7 @@ export default function Chemicals() {
           <Table.HeadCell>Freight Class</Table.HeadCell>
           <Table.HeadCell>Hazardous</Table.HeadCell>
           <Table.HeadCell>Hazard ID</Table.HeadCell>
-          <Table.HeadCell>Packing Group</Table.HeadCell>  
+          <Table.HeadCell>Packing Group</Table.HeadCell>
           <Table.HeadCell>
             <span className="sr-only">Edit</span>
           </Table.HeadCell>
@@ -107,31 +140,44 @@ export default function Chemicals() {
         <Table.Body className="divide-y">
           {data?.map((item, index) => (
             <Table.Row key={index} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="p-4">
-              <Checkbox />
-            </Table.Cell>
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {item.classificationId}
-            </Table.Cell>
-            <Table.Cell>{item.description}</Table.Cell>
-            <Table.Cell>{item.nmfc}</Table.Cell>
-            <Table.Cell>{item.sub || ''}</Table.Cell>
-            <Table.Cell>{item.freightClass}</Table.Cell>
-            <Table.Cell>{item.hazardous ? 'Yes' : 'No'}</Table.Cell>
-            <Table.Cell>{item.hazardId || ''}</Table.Cell>
-            <Table.Cell>{item.packingGroup || ''}</Table.Cell>
-            <Table.Cell>
-              <button onClick={() => { setOpenModal(true); setSelectedChemical(item); }}>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
-              </button>
-            </Table.Cell>
-          </Table.Row>
-            )
+              <Table.Cell className="p-4">
+                <Checkbox />
+              </Table.Cell>
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {item.classificationId}
+              </Table.Cell>
+              <Table.Cell>{item.description}</Table.Cell>
+              <Table.Cell>{item.nmfc}</Table.Cell>
+              <Table.Cell>{item.sub || ''}</Table.Cell>
+              <Table.Cell>{item.freightClass}</Table.Cell>
+              <Table.Cell>{item.hazardous ? 'Yes' : 'No'}</Table.Cell>
+              <Table.Cell>{item.hazardId || ''}</Table.Cell>
+              <Table.Cell>{item.packingGroup || ''}</Table.Cell>
+              <Table.Cell>
+                <button onClick={() => { setOpenModal(true); setSelectedChemical(item); }}>
+                  <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                    Edit
+                  </a>
+                </button>
+              </Table.Cell>
+            </Table.Row>
+          )
           )};
         </Table.Body>
       </Table>
     </div>
   );
 }
+
+
+
+{/* <div className="col-span-3 sm:col-span-1">
+                <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                  <option selected={true}>Select category</option>
+                  <option value="TV">TV/Monitors</option>
+                  <option value="PC">PC</option>
+                  <option value="GA">Gaming/Console</option>
+                  <option value="PH">Phones</option>
+                </select>
+              </div> */}
