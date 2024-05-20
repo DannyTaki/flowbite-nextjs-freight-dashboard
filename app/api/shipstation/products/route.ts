37b9b21938687adbca94a8e83de94138e1f42a7f
Daniel Taki <daniel@alliancechemical.com>
2024-05-20 15:32:03 -0500

    addProduct function

--- app/api/shipstation/products/route.ts
+++ app/api/shipstation/products/route.ts
@@ -1,11 +1,12 @@
 import { getMissingSKUs } from "@/app/actions/action"
 import { NextRequest, NextResponse } from "next/server";
+import { addProduct } from "@/app/actions/action";
 
 export async function POST() {
     try { 
         const missingProducts = await getMissingSKUs();
         if(missingProducts) {
-            updateProductTable(missingProducts);
+            addProduct(missingProducts);
         }
         
         return NextResponse.json(missingProducts);
