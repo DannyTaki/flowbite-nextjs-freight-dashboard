098b57fc8169efc9fbee0677f9682f7ee42c78d3
Daniel Taki <daniel@alliancechemical.com>
2024-05-24 09:35:11 -0500

    Added new page to appliciation

---
+++ app/(dashboard)/link/page.tsx
@@ -0,0 +1,11 @@
+import { getUnlinkedProducts } from "@/helpers/getData";
+import { useQuery } from "@tanstack/react-query";
+
+export default function UnlinkedProducts() {
+
+    return (
+        <>
+        
+        </>
+    )
+}
