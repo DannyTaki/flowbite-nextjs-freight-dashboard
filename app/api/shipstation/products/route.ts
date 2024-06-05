80e1b09cc318f235ccbd99df909d040db5da1551
Daniel Taki <daniel@alliancechemical.com>
2024-06-05 11:21:34 -0500

    added some more to the post function

--- app/api/shipstation/products/route.ts
+++ app/api/shipstation/products/route.ts
@@ -14,7 +14,7 @@ export async function POST() {
       })) || [];
       
       
-      addToProductFreightLinks(productsForFreightLinks);
+      await addToProductFreightLinks(productsForFreightLinks);
 
     }
 
