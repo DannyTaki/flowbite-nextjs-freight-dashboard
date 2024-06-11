c8771bec0fbbd129ae27c5be73c570337647c85e
Daniel Taki <daniel@alliancechemical.com>
2024-06-11 13:15:31 -0500

    added dropdown

--- app/(dashboard)/products/page.tsx
+++ app/(dashboard)/products/page.tsx
@@ -2,7 +2,7 @@
 
 import { getProducts, updateProduct } from "@/helpers/getData";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { Spinner, Table, Modal, Button } from "flowbite-react";
+import { Spinner, Table, Modal, Button, Dropdown } from "flowbite-react";
 import { useSearch } from "@/hooks/use-search";
 import { InsertProduct, SelectProduct } from "@/types/db/types";
 import { useState, useCallback } from "react";
@@ -19,6 +19,9 @@ const productSchema = z.object({
   unit_container_type: z.string().nullable(),
 });
 
+const packagingTypes = ["Bag", "Bale", "Box", "Bucket", "Bundle", "Carton", "Case", "Crate", "Cylinder", "Drums", "Pail", "Pallet", "Pieces", "Reel", "Roll", "Skid", "Tote", "Tube"];
+const unitContainerTypes = ["Bag", "Bale", "Box", "Bucket", "Bundle", "Carton", "Case", "Crate", "Cylinder", "Drums", "Pail", "Reel", "Roll", "Tote", "Tube"];
+
 export default function Products({
   searchParams,
 }: {
@@ -30,7 +33,6 @@ export default function Products({
   const query = searchParams?.query || '';
   const [openModal, setOpenModal] = useState(false);
   const [selectedChemical, setSelectedChemical] = useState<SelectProduct | null>(null);
-  const [localInput, setLocalInput] = useState<{ packaging_type?: string; unit_container_type?: string } | null>(null);
   const [showToast, setShowToast] = useState(false);
   const [toastMessage, setToastMessage] = useState<string | null>(null);
   const queryClient = useQueryClient();
@@ -43,21 +45,10 @@ export default function Products({
 
   const openEditModal = (item: SelectProduct) => {
     setOpenModal(true);
-    setLocalInput({
-      packaging_type: item.packaging_type ?? undefined,
-      unit_container_type: item.unit_container_type ?? undefined,
-    });
+    setSelectedChemical(item);
   };
 
-  const debouncedUpdateChemical = useDebouncedCallback((updatedChemical: SelectProduct) => {
-    setSelectedChemical(updatedChemical);
-  }, 100);
-
-  const handleInputChange = (field: string, value: string) => {
-    setLocalInput((prev) => (prev ? { ...prev, [field]: value } : { [field]: value }));
-  };
-
-  const handleFormSubmit = useCallback(async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setShowToast(false);
     console.log(selectedChemical);
@@ -90,7 +81,7 @@ export default function Products({
         }
       }
     }
-  }, [selectedChemical, queryClient]);
+  };
 
   if (error || productError) {
     console.log(error);
@@ -110,45 +101,31 @@ export default function Products({
   return (
     <div className="overflow-x-auto">
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
-        <Modal.Header></Modal.Header>
+        <Modal.Header>Edit Product</Modal.Header>
         <Modal.Body>
           <form className="p-4 md:p-5" onSubmit={handleFormSubmit}>
             <div className="mb-4 grid grid-cols-2 gap-4">
               <div>
-                <label
-                  htmlFor="Packaging Type"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Packaging Type
-                </label>
-                <input
-                  onChange={(e) => handleInputChange("packaging_type", e.target.value)}
-                  type="text"
-                  name="packaging_type"
-                  id="packaging_type"
-                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
-                  placeholder="Type the packaging type"
-                  required={true}
-                  value={localInput?.packaging_type || ""}
-                />
-              </div>
+                <Dropdown label="Packaging Type" className="mb-2" dismissOnClick={false}>
+                  {packagingTypes.map((option) => (
+                    <Dropdown.Item key={option} onClick={() => {
+                      if (selectedChemical) {
+                        setSelectedChemical({ ...selectedChemical, packaging_type: option})
+                      }
+                    }}>{option}</Dropdown.Item>
+                  ))}
+                  </Dropdown>
+                </div>
               <div>
-                <label
-                  htmlFor="Unit Container Type"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Unit Container Type
-                </label>
-                <input
-                  onChange={(e) => handleInputChange("unit_container_type", e.target.value)}
-                  type="text"
-                  name="unit_container_type"
-                  id="unit_container_type"
-                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
-                  placeholder="Type the packaging type"
-                  required={true}
-                  value={localInput?.unit_container_type || ""}
-                />
+              <Dropdown label="Unit Container Type" className="mb-2" dismissOnClick={false}>
+                {unitContainerTypes.map((option) => (
+                  <Dropdown.Item key={option} onClick={() => {
+                    if (selectedChemical) {
+                      setSelectedChemical({ ...selectedChemical, unit_container_type: option});
+                    }
+                  }}>{option}</Dropdown.Item>
+                ))}
+              </Dropdown>
               </div>
             </div>
             <Modal.Footer className="flex justify-center">
@@ -174,13 +151,6 @@ export default function Products({
           ))}
         </Table.Body>
       </Table>
-      <ProductModal
-        show={openModal}
-        onClose={() => setOpenModal(false)}
-        localInput={localInput}
-        handleInputChange={handleInputChange}
-        handleFormSubmit={handleFormSubmit}
-        />
     </div>
   );
 }
