98797cf7db63814a30b6101599e5675c838473d9
Daniel Taki <daniel@alliancechemical.com>
2024-06-17 16:19:20 -0500

    ran introspect

--- app/db/drizzle/relations.ts
+++ app/db/drizzle/relations.ts
@@ -1,21 +1,21 @@
 import { relations } from "drizzle-orm/relations";
-import { products, product_freight_links, freight_classifications } from "./schema";
+import { freight_classifications, product_freight_links, products } from "./schema";
 
 export const product_freight_linksRelations = relations(product_freight_links, ({one}) => ({
-	product: one(products, {
-		fields: [product_freight_links.product_id],
-		references: [products.product_id]
-	}),
 	freight_classification: one(freight_classifications, {
 		fields: [product_freight_links.classification_id],
 		references: [freight_classifications.classification_id]
 	}),
-}));
-
-export const productsRelations = relations(products, ({many}) => ({
-	product_freight_links: many(product_freight_links),
+	product: one(products, {
+		fields: [product_freight_links.product_id],
+		references: [products.product_id]
+	}),
 }));
 
 export const freight_classificationsRelations = relations(freight_classifications, ({many}) => ({
+	product_freight_links: many(product_freight_links),
+}));
+
+export const productsRelations = relations(products, ({many}) => ({
 	product_freight_links: many(product_freight_links),
 }));
--- app/db/drizzle/schema.ts
+++ app/db/drizzle/schema.ts
@@ -1,71 +1,54 @@
-import {
-  boolean,
-  integer,
-  numeric,
-  pgTable,
-  serial,
-  text,
-  unique,
-  uuid,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { pgTable, unique, serial, varchar, text, uuid, foreignKey, integer, numeric, boolean } from "drizzle-orm/pg-core"
+  import { sql } from "drizzle-orm"
 
-export const products = pgTable(
-  "products",
-  {
-    product_id: serial("product_id").primaryKey().notNull(),
-    sku: varchar("sku", { length: 255 }).notNull(),
-    name: text("name").notNull(),
-    packaging_type: varchar("packaging_type", { length: 255 }),
-    unit_container_type: varchar("unit_container_type", { length: 255 }),
-    objectID: uuid("objectID").defaultRandom(),
-  },
-  (table) => {
-    return {
-      products_sku_key: unique("products_sku_key").on(table.sku),
-    };
-  },
-);
 
-export const product_freight_links = pgTable(
-  "product_freight_links",
-  {
-    link_id: serial("link_id").primaryKey().notNull(),
-    product_id: integer("product_id")
-      .notNull()
-      .references(() => products.product_id),
-    classification_id: integer("classification_id").references(
-      () => freight_classifications.classification_id,
-    ),
-    objectID: uuid("objectID").defaultRandom().notNull(),
-  },
-  (table) => {
-    return {
-      unique_product_id: unique("unique_product_id").on(table.product_id),
-    };
-  },
-);
+
+export const products = pgTable("products", {
+	product_id: serial("product_id").primaryKey().notNull(),
+	sku: varchar("sku", { length: 255 }).notNull(),
+	name: text("name").notNull(),
+	packaging_type: varchar("packaging_type", { length: 255 }),
+	unit_container_type: varchar("unit_container_type", { length: 255 }),
+	objectID: uuid("objectID").defaultRandom(),
+},
+(table) => {
+	return {
+		products_sku_key: unique("products_sku_key").on(table.sku),
+	}
+});
+
+export const product_freight_links = pgTable("product_freight_links", {
+	link_id: serial("link_id").primaryKey().notNull(),
+	product_id: integer("product_id").notNull().references(() => products.product_id, { onDelete: "cascade" } ),
+	classification_id: integer("classification_id").references(() => freight_classifications.classification_id),
+	objectID: uuid("objectID").defaultRandom().notNull(),
+},
+(table) => {
+	return {
+		unique_product_id: unique("unique_product_id").on(table.product_id),
+	}
+});
 
 export const freight_classifications = pgTable("freight_classifications", {
-  classification_id: serial("classification_id").primaryKey().notNull(),
-  description: text("description"),
-  nmfc: varchar("nmfc", { length: 255 }),
-  freight_class: numeric("freight_class"),
-  hazardous: boolean("hazardous"),
-  hazard_id: varchar("hazard_id", { length: 255 }),
-  packing_group: varchar("packing_group", { length: 255 }),
-  sub: varchar("sub", { length: 255 }),
-  hazard_class: varchar("hazard_class", { length: 255 }),
-  objectID: uuid("objectID").defaultRandom(),
+	classification_id: serial("classification_id").primaryKey().notNull(),
+	description: text("description"),
+	nmfc: varchar("nmfc", { length: 255 }),
+	freight_class: numeric("freight_class"),
+	hazardous: boolean("hazardous"),
+	hazard_id: varchar("hazard_id", { length: 255 }),
+	packing_group: varchar("packing_group", { length: 255 }),
+	sub: varchar("sub", { length: 255 }),
+	hazard_class: varchar("hazard_class", { length: 255 }),
+	objectID: uuid("objectID").defaultRandom(),
 });
 
 export const product_freight_linkages = pgTable("product_freight_linkages", {
-  product_id: integer("product_id"),
-  sku: varchar("sku", { length: 255 }),
-  name: text("name"),
-  packaging_type: varchar("packaging_type", { length: 255 }),
-  unit_container_type: varchar("unit_container_type", { length: 255 }),
-  classification_id: integer("classification_id"),
-  link_id: integer("link_id"),
-  objectID: uuid("objectID"),
+	product_id: integer("product_id"),
+	sku: varchar("sku", { length: 255 }),
+	name: text("name"),
+	packaging_type: varchar("packaging_type", { length: 255 }),
+	unit_container_type: varchar("unit_container_type", { length: 255 }),
+	classification_id: integer("classification_id"),
+	link_id: integer("link_id"),
+	objectID: uuid("objectID"),
 });
